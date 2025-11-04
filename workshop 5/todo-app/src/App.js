70c97fb6c17c5a9a838fd66d4c0900b760ab727a
SamriddhiS2 <sivakumar.samriddhi@gmail.com>
2025-11-04 13:30:25 -0800

    initialize activity for workshop 5

---
+++ workshop 5/todo-app/src/App.js
@@ -0,0 +1,20 @@
+// TODO: what do we import here?
+
+function App() {
+  // TODO: what state variables do we need to store?
+
+  
+  // TODO: how do we fetch some pre-existing to-do items to load into our list on page load?
+
+
+  // TODO: what methods do we need to run?
+  
+
+  return (
+    <>
+      {/* TODO: what do we need to output? */}
+    </>
+  );
+}
+
+export default App;
