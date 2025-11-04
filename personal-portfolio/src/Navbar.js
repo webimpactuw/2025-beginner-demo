2c5b14245f3af0022b294c2a1546f2dc61f5a50d
SamriddhiS2 <sivakumar.samriddhi@gmail.com>
2025-11-04 14:07:20 -0800

    create a new folder for personal portfolio website

---
+++ personal-portfolio/src/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>Workshop</h1>
+      <ul>
+        <li><a href="home">Home</a></li>
+        <li><a href="resume">Resume</a></li>
+        <li><a href="contacts">Contacts</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
